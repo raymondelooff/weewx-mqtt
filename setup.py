691f93915617feb7ebdc8cb31928948ea7e7d585
Raymon de Looff <raydelooff@gmail.com>
2018-12-30 21:02:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018-2019 Raymon de Looff <raydelooff@gmail.com>
+# This extension is open-source software licensed under the GPLv3 license.
+
+__version__ = 0.1
+
+
+from setup import ExtensionInstaller
+
+
+def loader():
+    "Installs the WeeWX MQTT extension."
+    return MQTTInstaller()
+
+
+class MQTTInstaller(ExtensionInstaller):
+    "Installs the WeeWX MQTT extension."
+
+    def __init__(self):
+        super(MQTTInstaller, self).__init__(
+            version=__version__,
+            name='mqtt',
+            description='Send weather data to an MQTT broker.',
+            author='Raymon de Looff',
+            author_email='raydelooff@gmail.com',
+            restful_services='user.mqtt.MQTT',
+            config={
+                'StdRESTful': {
+                    'MQTT': {
+                        'enable': 'True',
+                        'host': 'localhost',
+                        'port': '1883',
+                        'username': 'weewx',
+                        'password': 'weewx'
+                    }
+                }
+            },
+            files=[('bin/user', ['bin/user/mqtt.py'])]
+        )
